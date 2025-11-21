c37398787af10c92ce09108cbca5e13f07f83f1f
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2025-11-21 11:37:56 +0700

    membuat page latihan

--- lib/page/page_latihan1.dart
+++ lib/page/page_latihan1.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_card_week8/main.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
